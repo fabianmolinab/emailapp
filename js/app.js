2a7769dcc6265911407322d3590052ed2bd04a42
fabianmolinab <molinafabian11b@gmail.com>
2020-06-18 18:14:18 -0500

    Validación de todos los campos

--- js/app.js
+++ js/app.js
@@ -33,6 +33,45 @@ function inicioApp() {
 
 function validarCampo() {
     
-    //Valida la longitud del texto y que no este vacio
+    //Valida la longitud del texto del campo actual 
+    validarLongitud(this)
+
+    //Validar el email 
+
+    if(this.type === 'email'){
+        validarMail(this)
+    }
+
+    //Valida que no este vacio y que no tenga errores
+    let errores = document.querySelectorAll('.error')
     
+    if(email.value !== '' && asunto.value !== '' && mensaje.value !== '') {
+
+        if(errores.length === 0){
+            btnEnviar.disabled = false
+        }
+    }
+}
+
+function validarLongitud(campo) {
+
+    if(campo.value.length >0){
+        campo.style.borderBottomColor = 'green'
+        campo.classList.remove('error')
+    }else {
+        campo.style.borderBottomColor = 'red'
+        campo.classList.add('error')
+    }
+}
+
+function validarMail(campo){
+    
+    const mensaje = campo.value
+    if (mensaje.indexOf('@') !== -1) {
+        campo.style.borderBottomColor = 'green'
+        campo.classList.remove('error')
+    }else {
+        campo.style.borderBottomColor = 'red'
+        campo.classList.add('error')
+    } 
 }
